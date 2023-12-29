9442b4b091db943cd1161e6391a1b9c25a9391d3
saiprasanna <saiiprasanna12@gmail.com>
2023-12-29 22:35:40 +0530

    m2

---
+++ cart.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+echo "script started executed at $TIMESTAMP"  &>> LOGFILE
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+VALIDATE()
+{
+  if [ $1 -ne 0 ]
+  then
+       echo -e  "installing $2 is $R failed $N" &>> LOGFILE
+  else 
+       echo -e "installing $2 is $G success $N"
+  fi
+}
+
+if [ $ID -ne 0]
+then 
+     echo -e " $R ERROR:please run with root user $N"  &>> LOGFILE
+     exit 1
+else
+    echo -e "$G you are in root user $N"
+fi
+
+yum install mysql -y  &>> LOGFILE
+
+VALIDATE $? "MYSQL"
+
+yum istall git -y  &>> LOGFILE
+
+VALIDATE $? "GIT"
+
+
