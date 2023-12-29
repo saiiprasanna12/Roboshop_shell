df945f85f6c84df6413332094c1f91c1196cfebf
saiprasanna <saiiprasanna12@gmail.com>
2023-12-29 16:19:26 +0530

    m2

---
+++ Mongodb.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOG_FILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started excecuted at $TIMESTAMP" &>> $LOG_FILE
+
+VALIDATE()
+{
+if [ $1 -ne 0 ]
+then
+
+echo -e "$2  is $R failed $N"
+exit1
+
+else
+
+echo -e "$2  is $G success $N"
+fi
+}
+
+
+if [ $ID -ne 0 ]
+then
+     echo -e "$R ERROR: please run with root user $N"
+     exit 1
+else
+     echo "you are in root user"
+fi
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+VALIDATE $? "copied to mongodb repo"
+
+
+
+
+
+
+
